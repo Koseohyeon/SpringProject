e50d18a5294bd5a10d10079445ff1c7e2ed29859
Koseohyeon <ksh030818@naver.com>
2024-05-28 17:58:41 +0900

    05.28수업중22

--- src/main/java/inhatc/cse/spring/controller/MemberController.java
+++ src/main/java/inhatc/cse/spring/controller/MemberController.java
@@ -2,66 +2,72 @@ package inhatc.cse.spring.controller;
 
 import inhatc.cse.spring.controller.dto.MemberDto;
 import inhatc.cse.spring.service.MemberService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.w3c.dom.ls.LSInput;
 
 import javax.servlet.http.HttpSession;
+import java.lang.reflect.Member;
 import java.util.List;
 
 @Controller
 @Slf4j
-@RequestMapping("/member")
 @RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
+
 
     private final MemberService memberService;
 
-    //@RequestMapping(value = "/member/save",method = RequestMethod.GET)
     @GetMapping("/save")
     public String save(){
-        // System.out.println("===================회원 가입 처리중....");
-        log.info("==================회원 가입 처리중");
+        log.info("===================회원 가입 처리중");
         return "member/save";
     }
 
     @PostMapping("/save")
-    public String memberInsert(@RequestBody MemberDto memberDto){
-        System.out.println("========="+ memberDto);
+    public String memberInsert(MemberDto memberDto){
+        System.out.println("================" + memberDto);
         int result = memberService.save(memberDto);
         if(result > 0){
+
             return "member/login";
-        } else {
+        }else {
             return "member/save";
         }
-    }
 
+    }
     @GetMapping("/login")
-    public String loginForm(){
+    public String LoginForm(){
+
         return "member/login";
     }
+
     @PostMapping("/login")
     public String login(@ModelAttribute MemberDto memberDto,
                         HttpSession session){
-        System.out.println("memberDto = "+ memberDto);
         boolean result = memberService.login(memberDto);
+
         if(result){
-            session.setAttribute("loginEmail", memberDto.getEmail());
+            session.setAttribute("loginEmail",memberDto.getEmail());
             return "main";
-        }else{
+        }else {
             return "member/login";
         }
     }
 
     @GetMapping("/list")
     public String memberList(Model model){
-        List<MemberDto> memberList=memberService.findAll();
-        System.out.println("=======>"+ memberList);
-        model.addAttribute("memberList",memberList);
+        List<MemberDto> memberList = memberService.findAll();
+        System.out.println("========>" + memberList);
+        model.addAttribute("memberList", memberList);
+
+
         return "member/list";
     }
-
 }

86f6911b08a484f9aa396396eb41863fca1ec9be
Koseohyeon <ksh030818@naver.com>
2024-06-04 17:15:07 +0900

    06.04 수업중22

--- src/main/java/inhatc/cse/spring/controller/MemberApiController.java
+++ src/main/java/inhatc/cse/spring/controller/MemberApiController.java
@@ -3,14 +3,18 @@ package inhatc.cse.spring.controller;
 import inhatc.cse.spring.controller.dto.MemberDto;
 import inhatc.cse.spring.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @CrossOrigin
+@Slf4j
 public class MemberApiController {
 
     private final MemberService memberService;
@@ -28,6 +32,13 @@ public class MemberApiController {
         System.out.println("========="+ memberDto);
         int result = memberService.save(memberDto);
         return new ResponseEntity<>(memberDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/memberlist")
+    public ResponseEntity<?>memberList(){
+        List<MemberDto> memberList=memberService.findAll();
+        System.out.println("memberList :"+ memberList);
+        return new ResponseEntity<>(memberList,HttpStatus.OK);
     }
 }
 

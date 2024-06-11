331e8d96689b9db4ddb00e7e34a2a9853f338f92
Koseohyeon <ksh030818@naver.com>
2024-06-11 13:36:26 +0900

    사진업로드
    사진업로드

--- src/main/java/inhatc/cse/spring/config/CorsConfig.java
+++ src/main/java/inhatc/cse/spring/config/CorsConfig.java
@@ -7,8 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3000/bookList")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000","http://localhost:300/bookList")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
